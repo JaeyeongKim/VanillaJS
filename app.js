89b0f76e037c4507fd21dc6d7ec1aba4d73b92e6
unknown <ddi03231@g.skku.edu>
2022-12-26 06:09:39 +0900

    쿼리셀렉터,스트링규칙

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+// const input = document.querySelector("#login-form input")
+const HIDDEN_CLASSNAME ="hidden";
 
-function handleBtnClick(){
-    const value = loginInput.value;
-    if(value === "kim"){
-        console.log("yes");
-    }else{
-        console.log("no");
-    }
+
+function handleBtnClick(event){
+    event.preventDefault(); //기본동작을 막는 함수, 새로고침을 막는다
+
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    /* greeting.innerText = "Hello " + username; */
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    console.log(username);
 }
 
 
-loginButton.addEventListener("click", handleBtnClick);
+
+
+loginForm.addEventListener("submit", handleBtnClick);
+
